4f5650fb3c7a1eaab4080694eeabf7ca2d9e114d
crocha12 <camilorocha11@gmail.com>
2022-09-16 11:28:17 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,19 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const res = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const resAccount = await accountCollection.findOne({ _id: res.insertedId }) as any
+    expect(resAccount.accessToken).toBeFalsy()
+    await sut.updateAccessToken(resAccount._id, 'any_token')
+    const account = await accountCollection.findOne({ _id: res.insertedId }) as any
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
+  })
 })
