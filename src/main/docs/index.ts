8c21c6319fe83fe437ffe5946d4d99076ba61cac
crocha12 <camilorocha11@gmail.com>
2022-09-26 09:16:07 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginParamsSchema } from './schemas/login-params-schema'
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API do curso de Node.js, Typescript, TDD, Clean Architecture e SOLID',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    login: loginParamsSchema
   }
 }
