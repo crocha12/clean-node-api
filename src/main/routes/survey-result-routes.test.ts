2358ee9b6c6ed62a583d38913175af32b73835d1
crocha12 <camilorocha11@gmail.com>
2022-09-28 08:21:55 -0300

    test: ensure load survey route returns 403 if no accessToken is provided

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -51,26 +51,34 @@ describe('Survey Routes', () => {
         })
         .expect(403)
     })
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      await request(app)
+        .put(`/api/surveys/${res.insertedId.toString()}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
+    })
   })
 
-  test('Should return 200 on save survey result with accessToken', async () => {
-    const accessToken = await makeAccessToken()
-    const res = await surveyCollection.insertOne({
-      question: 'Question',
-      answers: [{
-        answer: 'Answer 1',
-        image: 'http://image-name.com'
-      }, {
-        answer: 'Answer 2'
-      }],
-      date: new Date()
+  describe('GET  /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys/any_id/results')
+        .expect(403)
     })
-    await request(app)
-      .put(`/api/surveys/${res.insertedId.toString()}/results`)
-      .set('x-access-token', accessToken)
-      .send({
-        answer: 'Answer 1'
-      })
-      .expect(200)
   })
 })
